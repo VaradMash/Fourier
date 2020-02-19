2157b78d1b8e160e12ac84129d8427bdacf6cd0e
VaradMash <varadmash2201@gmail.com>
2020-02-19 11:48:21 +0530

    basic function written

--- Fourier.py
+++ Fourier.py
@@ -1,3 +1,14 @@
 from sympy import *
-x = Symbol('x')
-print(integrate(n*x))
+import numpy as np
+
+def gettheta(l,n):
+    global sintheta=[]
+    global costheta=[]
+    for i in range(6):
+        t=np.sin((l[i])*(np.pi/3))
+        c=np.cos((l[i])*(np.pi/3))
+        sintheta.append(t)
+        costheta.append(c)
+
+
+    
